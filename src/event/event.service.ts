dad36c072f1ead386383272da88d56daf1e8abdb
Adina <96437192+jecraftx@users.noreply.github.com>
2024-11-19 17:41:41 +0900

    Update event.service.ts

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -8,12 +8,6 @@ import { EventQuery } from './query/event.query';
 export class EventService {
   constructor(private readonly eventRepository: EventRepository) {}
 
-  // async findAllEvents():  Promise<EventListDto> {
-  //     const events: EventData[] = await this.eventRepository.findAllEvents();
-
-  //     return EventListDto.from(events);
-  // }
-
   async getEventById(eventId: number): Promise<EventDto> {
     const event = await this.eventRepository.getEventById(eventId);
     if (!event) {
